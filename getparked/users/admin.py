5f91c50fc473a7cb657c4e682cb8b5cb809003c7
Leo Baldock <leojdbaldock@gmail.com>
2019-03-23 17:02:22 +1000

    Added in password in admin

--- getparked/users/admin.py
+++ getparked/users/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from .models import User
+from .forms import CustomUserChangeForm
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+    list_display = ('id','email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    ordering = ('-is_staff', 'email',)
+    form = CustomUserChangeForm
---
+++ getparked/users/forms.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from .models import User
+
+
+class CustomUserChangeForm(UserChangeForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField(label=("Password"),
+                                         help_text=("Raw passwords are not stored, so there is no way to see "
+                                                    "this user's password, but you can change the password "
+                                                    "using <a href=\"../password/\">this form</a>."))
+
+    def __init__(self, *args, **kargs):
+        super(CustomUserChangeForm, self).__init__(*args, **kargs)
+
+    class Meta:
+        model = User
+        fields = '__all__'
